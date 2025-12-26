5c95f10cfe07ddb8fb80fefa35ef6ab08188a0f6
Mohamed Morsy <momooffline@proton.me>
2025-12-25 18:28:08 -0800

    Deploy with build

--- docs/build.sh
+++ docs/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ~/Python_Envs/bin/activate # Note for contributors: change this to YOUR venv if you have one
-python create_serach_md.py
+python create_search_md.py
 mkdocs build
 mkdocs gh-deploy
 git add .
